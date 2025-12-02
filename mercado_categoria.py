7c50d1b3dc20c59439e900820afb55eec38a3903
Leidiane Borgert <leidianeborgert@gmail.com>
2025-12-01 21:58:02 -0300

    criado arquivo de categorias

---
+++ mercado_categoria.py
@@ -0,0 +1,19 @@
+def executar():
+    #criar categoria()
+    #lista categoria()
+    #editar categoria()
+    #apagar categoria()
+    print('Obrigada')
+
+
+def criar_categoria():
+    pass
+
+def listar_categoria():
+    pass
+
+def editar_categoria():
+    pass
+
+def apagar_categoria():
+    pass
